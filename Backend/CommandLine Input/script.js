2bdce39fdb3904a85670eda720c7a672846a5690
Himanshu Yadav <himanshu.yadav.23cse@bmu.edu.in>
2025-06-23 23:20:43 +0530

    Taking User Input through terminal

---
+++ Backend/CommandLine Input/script.js
@@ -0,0 +1,15 @@
+console.log("Hello World")
+
+let a = 10;
+let b = 20;
+console.log(a+b);
+
+// console.log(process);
+// console.log(global.process);
+
+let x = parseInt(process.argv[2])
+let y = parseInt(process.argv[3])
+
+console.log('After adding =',x+y);
+
+console.log(process.argv)
